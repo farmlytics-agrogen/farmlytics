24779a869c7a7d7b0ffccfa9af31b3a005981b9f
farmlytics-agrogen <sec23ci008@sairamtap.edu.in>
2026-03-16 11:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+from flask import Flask, render_template
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Farmlytics AI Server Running"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
