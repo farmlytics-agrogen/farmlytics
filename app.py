2cf4447aade95a51f324eab1369b27cfd07412e1
farmlytics-agrogen <sec23ci008@sairamtap.edu.in>
2026-03-16 12:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
